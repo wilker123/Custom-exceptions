de60ae4f6d5e2ad218531909a1683d90a1ee3a5e
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-19 14:23:22 -0300

    Created custom exceptions project and implementation of account class and DomainExceptions class

---
+++ CustomExceptions/Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using CustomExceptions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; private set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawlimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawlimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance = amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainExceptions("The amount exceeds withdraw limit!!");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainExceptions("Not enough balance!!");
+            }
+        }
+    }
+}
---
+++ CustomExceptions/Exceptions/DomainExceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions.Exceptions
+{
+    class DomainExceptions : ApplicationException
+    {
+        public DomainExceptions(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
