8c8c8246dfc38f6677af7e8a7b84d088cc24085f
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-19 15:03:18 -0300

    Finalizing project implementing user interaction

--- CustomExceptions/Program.cs
+++ CustomExceptions/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CustomExceptions.Entities;
+using CustomExceptions.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,33 @@ namespace CustomExceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder,balance, withdrawLimit);
+                account.Deposit(balance);
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+
+                Console.WriteLine("New balance: " + account.Balance);
+            }
+            catch (DomainExceptions e)
+            {
+                Console.WriteLine("Error account: " + e.Message);
+            }
         }
     }
 }
